c08531e79790d97f89d88472fbb1da1496150b49
Pr00bit <marcin.idzik1@gmail.com>
2020-12-13 16:21:38 +0100

    Update Weathercheck.py

--- Weathercheck.py
+++ Weathercheck.py
@@ -3,8 +3,8 @@ import requests
 while True:
 	
 	print()
-	user_input = input('What is city name for which to check temperature: ')
-	response = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+user_input+'&APPID=eba1d2a48d8ef5168c95997d840b2fd2')
+	user_input = input('What is city name for which to check temperature: ')    # takes input from user for a city value
+	response = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+user_input+'&APPID=eba1d2a48d8ef5168c95997d840b2fd2') # connectcs to openweathermap.org API to get data
 
 	print()
 	print ('Establishing Connection to API.....')
@@ -16,7 +16,7 @@ while True:
 		 
 		jason_data = response.json()
 		
-		jason_temp = jason_data['main']['temp']
+		jason_temp = jason_data['main']['temp'] # extraction of temperature from Json
 
 		
 		celcius_temp = jason_temp - 273
